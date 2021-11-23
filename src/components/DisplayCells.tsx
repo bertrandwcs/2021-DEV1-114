2f30817304e6b34f2d0dd6fb65727171d672d552
CVBertrandCardon <bertrandcardondel@gmail.com>
2021-11-23 11:36:14 +0100

    display grid and scss

--- src/components/DisplayCells.tsx
+++ src/components/DisplayCells.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const DisplayCells = () => {
-  return <div className="cells-container">Display cells</div>;
+const DisplayCells = ({ index, cells }: any) => {
+  return (
+    <div className="cells-container">
+      <div className="game-grid-container">
+        <div className="cell">{cells[index]}</div>
+      </div>
+    </div>
+  );
 };
-
 export default DisplayCells;
--- src/pages/TicTacToePage.tsx
+++ src/pages/TicTacToePage.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import DisplayCells from "../components/DisplayCells";
 
 function TicTacToePage() {
+  const indexComponent = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const [cells, setCells] = useState(Array(9).fill(""));
+
   return (
     <div className="tic-tac-toe-container">
       <h1 data-testid="title">Tic tac toe</h1>
+      <div className="game-grid-container">
+        {indexComponent.map((elem) => {
+          return <DisplayCells index={elem} cells={cells} />;
+        })}
+      </div>
     </div>
   );
 }
